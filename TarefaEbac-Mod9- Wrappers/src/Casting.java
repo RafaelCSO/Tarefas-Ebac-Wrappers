c0661c37f117787c0e365d326e4279528bea31dd
RafaelCSO <rafaeljet00@gmail.com>
2023-04-05 19:22:40 -0300

    Wrappers mod9

---
+++ TarefaEbac-Mod9- Wrappers/src/Casting.java
@@ -0,0 +1,12 @@
+public class Casting {
+    public static void main(String args[]) {
+
+    int IdadeP = 16;
+    double NotaP = 10.5;
+
+    Integer Idade = IdadeP;
+    Double Nota = NotaP;
+    System.out.println("Idade " + Idade);
+    System.out.println("Nota " + Nota);
+    }
+}
